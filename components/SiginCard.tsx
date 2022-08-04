c588cdd858ea42ceb7c454697d6e0dbcc8bbd1c4
mat-garcia <mateussoap@gmail.com>
2022-08-04 02:04:07 -0300

    initial project and Signin front-end

---
+++ components/SiginCard.tsx
@@ -0,0 +1,30 @@
+export default function SigninCard() {
+    return (
+        
+    <div className="row box">
+        <div className="col s12 center-align ">
+          <div className="col l6 m6 s6">
+            <h2>Bem Vindo ao</h2>
+            <h2><b>Komander.</b></h2>
+            
+          </div>
+          <div className="col l6 m6 s6">
+            <div className="input-field">
+              <input id="user" type="text" className="validate" placeholder="Usuario" />
+              {/* <label htmlFor="user">Usuário</label> */}
+            </div>
+            <div className="input-field">
+              <input id="password" type="password" placeholder="senha" className="validate" />
+             {/*  <label htmlFor="password">Senha</label> */}
+              
+            </div>
+            <div className="input-field">
+                <button className="btn entrar waves-effect waves-light" type="submit" name="action">Entrar
+                    <i className="material-icons right">login</i>
+                </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+//import '../styles/globals.css'
+import "materialize-css/dist/css/materialize.min.css";
+//import "materialize-css/dist/js/materialize.min.js";
+import "../styles/css/material-icons.css";
+import "../styles/globals.css";
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
